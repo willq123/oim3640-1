d4f4a8a5d3d35372348ed9e08f89a43d0c08290b
Zhi Li <lzblack@users.noreply.github.com>
2024-09-04 17:54:47 -0400

    Finish session03

--- session03/hello.py
+++ session03/hello.py
@@ -1,1 +1,40 @@
-print('Hi')
+# print("Hello, world!")
+
+# print("Hey Jude", "don't make it bad")
+
+# # \ escape character
+
+print("The total number of overall medals in Paris 2024 is", 40 + 44 + 41)
+
+total = 40 + 44 + 41
+print(f"The total number of overall medals in Paris 2024 is {total}.")
+# print("40 + 44 + 41 =", 40 + 44 + 41)
+
+
+# name = input("What is your name? >> ")
+# print("Hello,", name)
+
+# print('Good bye,', name)
+
+
+# n = 100
+# n * 10  # this is an expression
+# print(n * 100)
+
+# a = 123
+
+# print(a)
+# print(type(a))
+
+# a = 'ABC'
+# print(a)
+# print(type(a))
+
+
+# x = 10
+# print(x)
+
+# x + 2
+# print(x)
+
+# print("Naah, na na nanana naah, nanana naah, hey Jude. \n" * 10)
