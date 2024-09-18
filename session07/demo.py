190cc6efcc0e042c39bdc9f171b3e152e7432e35
Zhi Li <lzblack@users.noreply.github.com>
2024-09-18 16:51:41 -0400

    Modify if demo

--- session07/demo.py
+++ session07/demo.py
@@ -1,8 +1,8 @@
 age = 22
 
-if age >= 18:
-    print("You are in college. You decide.")
 if age >= 21:
     print("Yes, you can!")
+elif age >= 18:
+    print("You are in college. You decide.")
 else:
     print("No, you cannot!")
