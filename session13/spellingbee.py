6af29b41f71ef63c0bd43514bbf2a624852ea3c6
Zhi Li <lzblack@users.noreply.github.com>
2024-10-09 17:09:28 -0400

    Add spellingbee.py in session 13

---
+++ session13/spellingbee.py
@@ -0,0 +1,69 @@
+# https://www.nytimes.com/puzzles/spelling-bee
+
+
+def at_least(word, n):
+    """Return True if the word has at least n letters"""
+    # if len(word) >= n:
+    #     return True
+    # else:
+    #     return False
+    return len(word) >= n
+
+
+def must_include(word, letter):
+    """Return True if the word contains the required letter"""
+    return letter in word
+
+
+def use_only(word, available):
+    """
+    Return True if the word uses only the available letters, return False if any letter in word is not from the available letters
+    """
+    # check each letter in word
+    #   if letter is not from the available letters:
+    #       return False
+    # After checking all the letters in word:
+    # return True
+    for letter in word:
+        if letter not in available:
+            return False
+    return True
+
+
+# print(use_only("babson", "abcd"))  # False
+# print(use_only("babson", "absonxyz"))  # True
+
+
+def solve(must_have, available):
+    """Solve spellingbee puzzle
+    Return all the qualified words as a list
+    """
+    f = open("data/words.txt")  # open the word list file
+    # f = open("data/words_test.txt")
+    # Iterate over the word list, check each word
+    #   if the word is qualified (at least 4 letters, must include the must_have letter, use only the available letters)
+    #        print the word
+    res = []
+    for line in f:
+        # print(line, len(line))
+        word = line.strip()  # Get each word after removing the ending '\n'
+        if (
+            at_least(word, 4)
+            and must_include(word, must_have)
+            and use_only(word, available)
+        ):
+            # print(word)
+            res.append(word)
+    return res
+
+
+def main():
+    # what information?
+    center_letter = "o"
+    other_letters = "atnhel"
+
+    words = solve(center_letter, other_letters + center_letter)  # with the information
+    print(words)
+
+
+main()
